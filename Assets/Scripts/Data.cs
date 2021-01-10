c171c88d4b8e5450293d45e964efc447f85c9e36
FromFire <yuanxiasummer@icloud.com>
2021-01-10 22:59:14 +0800

    优化目录；这电脑vscode不行，提交以转战笔记本

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    SaveEntity saveEntity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 设置不销毁
+        
+        // 读取存档文件
+        string savePath = "Save/save";
+        TextAsset text = Resources.Load<TextAsset>(savePath);
+        string json = text.text;
+        saveEntity = SaveEntity.fromJson(json);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SaveEntity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+// 已有卡牌情况
+[Serializable]
+public class CardEntity{
+    public string name;
+    public int number;
+}
+
+// 存档数据
+[Serializable]
+public class SaveEntity
+{
+    public int points;
+    public List<CardEntity> cards;
+
+    //转化为json格式
+    public string ToJson () {
+        return JsonUtility.ToJson(entity);
+    }
+
+    //读取自json格式
+    public static SaveEntity fromJson(string text) {
+        return JsonUtility.FromJson<SaveEntity>(text);
+    }
+}
